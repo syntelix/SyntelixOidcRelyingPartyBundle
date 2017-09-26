060081eddecbc1a73162ca29bde5c32ceea50f44
alberte <albertegz@gmail.com>
2017-09-26 10:30:34 +0200

    refactor: Remove unnecessary backwards compatibility

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,11 +1,5 @@
 <?php
 
-// Grant backwards compatibility to PHPUnit 6 tests
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
-	class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
-
 if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
 	$autoload = require_once $file;
 } else {
